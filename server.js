0433bd3a2415a3a02238ab46938fbec9eb598294
Daniel Meixner <daniel.meixner@live.com>
2020-03-20 16:50:36 +0100

    added bootstrap for beauty

--- server.js
+++ server.js
@@ -109,6 +109,7 @@ app.get('/api/cascade', function (req, res) {
                 var htmlhead = "" +
                     "<html>" +
                     "<head>" +
+                    "<link rel=\"stylesheet\" href=\"https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css\" integrity=\"sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh\" crossorigin=\"anonymous\">" +
                     "<style>" +
                     "canvas {float: left;width:100px;height:100px; }" +
                     'i {font-size:100px;}' +
